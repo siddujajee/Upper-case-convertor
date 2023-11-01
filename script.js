bc96c9e0bbbec6f9bbfefdb056ba769e63aa1238
siddujajee <146925563+siddujajee@users.noreply.github.com>
2023-11-01 15:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let messageinput= document.getElementById('message-input');
+messageinput.addEventListener('keydown', function(event){
+    if (event.key == "Enter") {
+        
+        getmessage();
+    }
+})
+function getmessage(){
+    document.getElementById('message-output').innerHTML= messageinput.value;
+    // messageinput.value=''
+}
